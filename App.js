f4f570b8f7cc49194e9c9cbdaf20a4a98a3d2ced
Chandrasekhar-koneti <konetichandra123@gmail.com>
2023-01-12 21:09:28 +0530

    Task3 adv-react

---
+++ App.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ExpenseItem from './components/ExpenseItem.js'
+function App() {
+  const expenses=[
+    {
+    date:new Date(2023,0,23),
+    title:"Car Insurance",
+    amount:'RS.1250'},
+
+    {
+      date:new Date(2023,1,8),
+      title:'Food',
+      amount:'RS.2000'
+    },
+
+    {
+      date:new Date(2022,11,28),
+      title:'Trip',
+      amount:'RS.82000'
+    },
+    {
+      date:new Date(2022,11,31),
+      title:'Fuel',
+      amount:'RS.8000'
+    }
+
+  ]
+//  expenses.map((expenses)=>(console.log(expenses)))
+
+
+  return (
+
+    <div>
+     {<h2> EXPENSE ITEMS</h2> }
+  
+     {expenses.map((expense,index)=>
+     
+     <ExpenseItem key={index}
+     date={expenses[index].date}
+     title={expenses[index].title }
+     amount={expenses[index].amount}>
+     </ExpenseItem>)}
+
+ 
+    </div>
+
+  );
+}
+
+export default App;
+
+
+
+
+
---
+++ components/ExpenseItem.js
@@ -0,0 +1,28 @@
+import ExpenseDate from './ExpenseDate'
+import './expense.css'
+import './ExpenseDate.css'
+import { useState } from 'react'
+function ExpenseItem(props){
+
+    const[amount,setamount]=useState(props.amount);
+
+    const clickhandler=()=>{
+        setamount('RS.100')
+        console.log('amount changed')
+    }
+
+    return ( 
+    <div className='expense-item'>
+        <ExpenseDate date={props.date}></ExpenseDate>
+
+        <div className='expense-item__description'><h2>{props.title}</h2> </div>
+
+        <div className='expense-item__price'>{amount}</div>
+        <button onClick={clickhandler}> change_amount</button>
+
+    
+    </div>)
+
+}
+
+export default ExpenseItem

280978870919e349b9c5f5b9ac60341a58cac59b
Chandrasekhar-koneti <konetichandra123@gmail.com>
2023-01-14 19:00:36 +0530

    task7 adv react

--- components/NewExpense/ExpenseForm.js
+++ components/NewExpense/ExpenseForm.js
@@ -1,29 +1,54 @@
 
-import React from "react"
+import React, {useState} from "react"
 
 import './ExpenseForm.css'
 
 const ExpenseForm=()=>{
+    
+    const [enteredTitle,setEnteredTitle]=useState('')
+    const [enteredamount,setEnteredAmount]=useState('')
+    const [enteredDate,setEnteredDate]=useState('')
 
-    const handler=(event)=>{
-        console.log(event.target.value)
+    const titlehandler=(event)=>{
+        setEnteredTitle(event.target.value)
+    
     }
 
-    return <form>
+    const amounthandler=(event)=>{
+        setEnteredAmount(event.target.value)
+    }
+
+    const datehandler=(event)=>{
+        setEnteredDate(event.target.value)
+    }
+
+    const submithandler=(event)=>{
+        event.preventDefault()
+
+        const expensedata={
+            title:enteredTitle,
+            amount:enteredamount,
+            date:new Date(enteredDate)
+        }
+
+        console.log(expensedata)
+    }
+
+    return <form onSubmit={submithandler}>
         <div className="new-expense__controls">
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" onChange={handler}/>
+                <input type="text" onChange={titlehandler}/>
             </div>
 
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" onChange={handler}/>
+                <input type="number" min='0' onChange={amounthandler}/>
             </div>
 
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="date" onChange={handler}/>
+                <input type="date" min='2020-01-01' max='2023-12-31' onChange={datehandler}/>
             </div>
         </div>
         <div className="new-expense__actions">

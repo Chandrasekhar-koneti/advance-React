e7a89193ba7b8c874ae143f85aceb4afa0a41bdb
Chandrasekhar-koneti <117808164+Chandrasekhar-koneti@users.noreply.github.com>
2023-01-17 15:53:31 +0530

    task11 adv react

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -12,11 +12,16 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpense=props.items.filter(expense=>{
+    return expense.date.getFullYear().toString()===filteredYear
+  })
+
   return (
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        {props.items.map((expense)=>(
+
+        {filteredExpense.map((expense)=>(
           <ExpenseItem key={expense.id}
           title={expense.title}
           amount={expense.amount}
